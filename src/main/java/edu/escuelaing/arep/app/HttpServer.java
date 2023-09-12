0c43a5661de157645d7198c083ddf66f5ef21782
unknown <juan.rodriguez-pen@labinfo.is.escuelaing.edu.co>
2023-09-12 15:46:15 -0500

    corre

--- src/main/java/edu/escuelaing/arep/app/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/app/HttpServer.java
@@ -12,58 +12,61 @@ public class HttpServer {
          System.err.println("Could not listen on port: 36000.");
          System.exit(1);
       }
-
-      Socket clientSocket = null;
-      try {
-         System.out.println("Listo para recibir ...");
-         clientSocket = serverSocket.accept();
-      } catch (IOException e) {
-         System.err.println("Accept failed.");
-         System.exit(1);
-      }
-      PrintWriter out = new PrintWriter(
-            clientSocket.getOutputStream(), true);
-      BufferedReader in = new BufferedReader(
-            new InputStreamReader(clientSocket.getInputStream()));
-      String inputLine, outputLine, uriString,firstLine;
-      uriString = "";
-      firstLine = "/";
-      while ((inputLine = in.readLine()) != null) {
-         if (inputLine.contains("consulta?comando=")) {
-            String[] ans = inputLine.split("comando=");
-            uriString = (ans[1].split("HTTP")[0]).replace(" ", "");
-         }
-         System.out.println("Recibí: " + inputLine);
-         if (!in.ready()) {
-            break;
-         }
-      }
-
       
-      
-      if (!uriString.equals("")) {
-         outputLine = "HTTP/1.1 200 OK\r\n"
-               + "Content-Type: text/html\r\n"
-               + "\r\n"
-               + "<!DOCTYPE html>\n"
-               + "<html>\n"
-               + "<head>\n"
-               + "<meta charset=\"UTF-8\">\n"
-               + "<title>Title of the document</title>\n"
-               + "</head>\n"
-               + "<body>\n"
-               + "<h1>Mi propio mensaje</h1>\n"
-               + "</body>\n"
-               + "</html>\n";
-      } else {
-         outputLine = "HTTP/1.1 200 OK\r\n"
-               + "Content-Type: text/html\r\n"
-               + "\r\n" + getResponse();
+      boolean running = true;
+      while (running) {
+         Socket clientSocket = null;
+         try {
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
+         } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+         }
+         PrintWriter out = new PrintWriter(
+               clientSocket.getOutputStream(), true);
+         BufferedReader in = new BufferedReader(
+               new InputStreamReader(clientSocket.getInputStream()));
+         String inputLine, outputLine, uriString, firstLine;
+         uriString = "";
+         firstLine = "";
+         outputLine = "";
+         while ((inputLine = in.readLine()) != null) {
+            if (inputLine.contains("consulta?comando=")) {
+               String[] ans = inputLine.split("comando=");
+               uriString = (ans[1].split("HTTP")[0]).replace(" ", "");
+            }
+            System.out.println("Recibí: " + inputLine);
+            
+            if (!in.ready()) {
+               break;
+            }
+         }
+
+         if (!uriString.equals("")) {
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                  + "Content-Type: text/html\r\n"
+                  + "\r\n"
+                  + "<!DOCTYPE html>\n"
+                  + "<html>\n"
+                  + "<head>\n"
+                  + "<meta charset=\"UTF-8\">\n"
+                  + "<title>Title of the document</title>\n"
+                  + "</head>\n"
+                  + "<body>\n"
+                  + "<h1>Mi propio mensaje</h1>\n"
+                  + "</body>\n"
+                  + "</html>\n";
+         } else {
+            outputLine = "HTTP/1.1 200 OK\r\n"
+                  + "Content-Type: text/html\r\n"
+                  + "\r\n" + getResponse();
+         }
+         out.println(outputLine);
+         out.close();
+         in.close();
+         clientSocket.close();
       }
-      out.println(outputLine);
-      out.close();
-      in.close();
-      clientSocket.close();
       serverSocket.close();
    }
 
@@ -71,7 +74,7 @@ public class HttpServer {
       String response = "<!DOCTYPE html>\n"
             + "<html>\n"
             + "    <head>\n"
-            + "        <title>Form Example</title>\n"
+            + "        <title>Reflective ChatGPT</title>\n"
             + "        <meta charset=\"UTF-8\">"
             + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
             + "    </head>\n"
